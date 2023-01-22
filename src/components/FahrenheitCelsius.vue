451fb9bde860f738b973f394405e1db6f384c5ae
SunbearDay <sunbearday@outlook.com>
2023-01-21 21:23:58 -0700

    save

--- src/components/FahrenheitCelsius.vue
+++ src/components/FahrenheitCelsius.vue
@@ -3,19 +3,20 @@ function convertFahrenheitToCelsius(fahrenheit: number): number {
   return (fahrenheit - 32) / 1.8
 }
 
-function updateCelsius(event: Event) {
-  let elCelsius = document.querySelector('#celsius') as HTMLInputElement
-  let celsius = convertFahrenheitToCelsius((event.target as HTMLInputElement).valueAsNumber)
-  // let f = (event.target as HTMLInputElement).valueAsNumber
-  // console.log(typeof f)
-  // let celsius = document.querySelector('#celsius') as HTMLInputElement
-  // celsius.value = formatConversion(((target.valueAsNumber - 32) / 1.8))
+function convertCelsiusToFahrenheit(celsius: number): number {
+  return ((celsius * 1.8) + 32)
 }
 
-function updateFahrenheit(event: Event) {
-  let target = event.target as HTMLInputElement
-  let fahrenheit = document.querySelector('#fahrenheit') as HTMLInputElement
-  fahrenheit.value = formatConversion(((target.valueAsNumber * 1.8) + 32))
+function updateCelsiusInput(event: Event) {
+  let elCelsius = document.querySelector('#celsius') as HTMLInputElement
+  let celsius = convertFahrenheitToCelsius((event.target as HTMLInputElement).valueAsNumber)
+  elCelsius.value = formatConversion(celsius)
+}
+
+function updateFahrenheitInput(event: Event) {
+  let elFahrenheit = document.querySelector('#fahrenheit') as HTMLInputElement
+  let fahrenheit = convertCelsiusToFahrenheit((event.target as HTMLInputElement).valueAsNumber)
+  elFahrenheit.value = formatConversion(fahrenheit)
 }
 
 function formatConversion(value: number): string {
@@ -33,13 +34,28 @@ function formatConversion(value: number): string {
       <div>
         <label for="fahrenheit">Fahrenheit</label>
         <input type="number" name="fahrenheit" id="fahrenheit" step="0.1"
-          @input="updateCelsius($event)">
+          @input="updateCelsiusInput($event)">
       </div>
       <div>
         <label for="celsius">Celsius</label>
         <input type="number" name="celsius" id="celsius" step="0.1"
-          @input="updateFahrenheit($event)">
+          @input="updateFahrenheitInput($event)">
       </div>
+    </article>
+
+    <article class="info">
+      <p>A quick, but not so accurate way to convert fahrenheit to celsius is to subtract 30 from the fahrenheit temperature and then divide by two.</p>
+    </article>
+
+    <article class="info">
+      <p>Here's two formulas to calculate fahrenheit to celsius:</p>
+      <p>C = 5/9 x (F - 32)</p>
+      <p>C = (F - 32) / 1.8</p>
+    </article>
+
+    <article class="info">
+      <p>Here's the formula to calculate celsius to fahrenheit:</p>
+      <p>F = (C x 1.8) + 32</p>
     </article>
   </div>
 </template>
@@ -49,17 +65,29 @@ function formatConversion(value: number): string {
   margin-block: 1rem;
 }
 
-article {
-  padding-block: 1em;
+.conversion-input {
   display: flex;
-  gap: 1rem;
   flex-wrap: wrap;
+  gap: 2rem;
   justify-content: center;
+  margin-bottom: 1rem;
+  padding-block: 1em;
 }
 
-article>div {
+.conversion-input > div {
   display: grid;
   gap: 0.3em;
   text-align: center;
+}
+
+input {
+  max-width: 10ch;
+}
+
+input[type=number] {
+  -moz-appearance: textfield;
+}
+.info {
+  margin-bottom: 1rem;
 }
 </style>
